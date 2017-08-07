bcc48f9e34105f695dba8e1bc00881b8683916da
Yehuda Katz <wycats@gmail.com>
2017-08-07 14:15:08 -0700

    Restructure the tests so they used a test class
    This structure still uses the QUnit test harness but takes better
    advantage of TypeScript improvements that require the use of classes.
    
    Notably, this also allows us to have instance state on our tests and
    methods that use that instance state.
    
    In this commit, I moved the `env` module-global variable into the
    `ValidationTest` superclass, which then allowed me to create a
    `validate` method that uses `this.env`.
    
    In practice, we expect that an Ember wrapper for this library would
    create an API (either exported functions or an exported object) that
    constructed the Ember environment and exposed validation function that
    used that environment. As a reminder, at the moment, the environment
    encapsulates the `Ember.get` vs. pure `[]` distinction as well as the
    validation registration mechanism (which, in Ember, would be via the
    resolver/container).
    
    The benefit of this structure is that we could write validators once
    that are agnostic to these concerns and use them both inside of Ember
    (with Ember objects) and outside of Ember (with pure POJOs and no Ember
    runtime present). In an ideal world, we could even get non-Ember users
    to help maintain the validators.

--- test/validation-test.ts
+++ test/validation-test.ts
@@ -1,70 +1,72 @@
 import { SingleFieldValidator, Opaque, SingleFieldError } from '@validations/core';
 import dsl, { validates } from '@validations/dsl';
 
-import { moduleForValidations, validator, validate } from './support';
+import { ValidatorDecorator, ValidationTest, QUnitAssert, module, test } from './support';
 
-const { test } = QUnit;
+@module("Single Field Validators")
+export class ValidatorTest extends ValidationTest {
+  @test
+  async "a presence validator"(assert: QUnitAssert) {
+    let descriptors = dsl({
+      name: validates('presence')
+    });
 
-moduleForValidations('Validators');
+    let failure = [
+      { path: ['name'], message: 'presence' }
+    ];
 
-function validators() {
-  @validator('presence')
-  class PresenceValidator extends SingleFieldValidator<ReadonlyArray<never>> {
-    validate(value: Opaque, error: SingleFieldError): void {
-      if (value === null || value === undefined) {
-        error.set('presence');
-      }
-    }
+    assert.deepEqual(await this.validate(null, descriptors), failure, 'validate(null)');
+    assert.deepEqual(await this.validate({}, descriptors), failure, 'validate({})');
+
+
+    assert.deepEqual(await this.validate({ name: 'hello' }, descriptors), [], `validate({ name: 'hello' })`);
+    assert.deepEqual(await this.validate({ name: null }, descriptors), failure, `validate({ name: null })`);
   }
 
-  @validator('length')
-  class LengthValidator extends SingleFieldValidator<[{ min?: number, max?: number }]> {
-    validate(_value: Opaque, error: SingleFieldError): void {
-      let length = this.getSubProperty('length');
+  @test
+  async "a length validator"(assert: QUnitAssert) {
+    let descriptors = dsl({
+      emails: [
+        validates('presence'),
+        validates('length', { min: 1 })
+      ]
+    });
 
-      if (typeof length === 'number') {
-        let [ { min = 0, max = Infinity } ] = this.args;
+    assert.deepEqual(await this.validate(null, descriptors), [{ path: ['emails'], message: 'presence' }]);
+    assert.deepEqual(await this.validate({ emails: null }, descriptors), [{ path: ['emails'], message: 'presence' }]);
+    assert.deepEqual(await this.validate({ emails: [] }, descriptors), [{ path: ['emails'], message: 'length' }]);
+    assert.deepEqual(await this.validate({ emails: ["wycats@example.com"] }, descriptors), []);
+  }
 
-        if (length < min || length > max) {
-          error.set('length');
+  // The `any` here and the return in the body of the function is working around a typescript
+  // limitation that prevents us from decorating a class expression.
+  //
+  // see https://github.com/Microsoft/TypeScript/issues/9448#issuecomment-320779210
+  protected define(validator: ValidatorDecorator): any {
+    @validator('presence')
+    class PresenceValidator extends SingleFieldValidator<ReadonlyArray<never>> {
+      validate(value: Opaque, error: SingleFieldError): void {
+        if (value === null || value === undefined) {
+          error.set('presence');
         }
       }
     }
+
+    @validator('length')
+    class LengthValidator extends SingleFieldValidator<[{ min?: number, max?: number }]> {
+      validate(_value: Opaque, error: SingleFieldError): void {
+        let length = this.getSubProperty('length');
+
+        if (typeof length === 'number') {
+          let [ { min = 0, max = Infinity } ] = this.args;
+
+          if (length < min || length > max) {
+            error.set('length');
+          }
+        }
+      }
+    }
+
+    return { PresenceValidator, LengthValidator };
   }
-
-  return { PresenceValidator, LengthValidator };
 }
-
-test("a presence validator", async assert => {
-  validators();
-
-  let descriptors = dsl({
-    name: validates('presence')
-  });
-
-  let failure = [
-    { path: ['name'], message: 'presence' }
-  ];
-
-  assert.deepEqual(await validate(null, descriptors), failure, 'validate(null)');
-  assert.deepEqual(await validate({}, descriptors), failure, 'validate({})');
-
-  assert.deepEqual(await validate({ name: 'hello' }, descriptors), [], `validate({ name: 'hello' })`);
-  assert.deepEqual(await validate({ name: null }, descriptors), failure, `validate({ name: null })`);
-});
-
-test("a length validator", async assert => {
-  validators();
-
-  let descriptors = dsl({
-    emails: [
-      validates('presence'),
-      validates('length', { min: 1 })
-    ]
-  });
-
-  assert.deepEqual(await validate(null, descriptors), [{ path: ['emails'], message: 'presence' }]);
-  assert.deepEqual(await validate({ emails: null }, descriptors), [{ path: ['emails'], message: 'presence' }]);
-  assert.deepEqual(await validate({ emails: [] }, descriptors), [{ path: ['emails'], message: 'length' }]);
-  assert.deepEqual(await validate({ emails: ["wycats@example.com"] }, descriptors), []);
-});
