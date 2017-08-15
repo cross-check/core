1e76c1a31efc4c121061bd7131bbdfee3349c60d
Dan DeGreef <daniel_degreef@condenast.com>
2017-08-15 11:42:27 -0500

    add year validator

--- src/validators.ts
+++ src/validators.ts
@@ -3,3 +3,4 @@ export * from './validators/obj';
 export * from './validators/single-field';
 export * from './validators/url';
 export * from './validators/email';
+export * from './validators/year';
